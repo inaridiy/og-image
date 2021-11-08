8c3f0b6833366ebc9fa5f4db74ca9f70e54200a0
inaridiy <inainainariinainari@gmail.com>
2021-11-08 12:17:48 +0900

    修正

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -101,7 +101,7 @@ function getCss(theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter', sans-serif;
+        font-family: 'Noto Sans JP', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: ${foreground};
